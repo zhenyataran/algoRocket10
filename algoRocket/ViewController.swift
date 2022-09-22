b65cc6ec8e424b23d5d84d3ae095058f1880110d
Zhenya Taran <drakzon1@gmail.com>
2022-09-22 23:29:27 +0300

    Initial Commit

---
+++ algoRocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  algoRocket
+//
+//  Created by Евгений Таран on 22.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
